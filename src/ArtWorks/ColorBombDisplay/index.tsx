b597f34a43a63702296a2003e2b8a7b3e9852db6
aman-can <aman.dubey1998@gmail.com>
2023-04-30 00:25:46 +0530

    🌈 Added the a art piece call color bomb display

--- src/ArtWorks/ColorBombDisplay/index.tsx
+++ src/ArtWorks/ColorBombDisplay/index.tsx
@@ -1,3 +1,6 @@
+import { useEffect, useRef, useState } from "react";
+import { nanoid } from "nanoid";
+
 const colors = [
 	"#FFCE2E",
 	"#FB685A",
@@ -5,10 +8,120 @@ const colors = [
 	"#4526DB",
 	"#0093D9",
 	"#00E9A8"
-];
+] as const;
 
-console.log(colors);
+type ColorIndex = 0 | 1 | 2 | 3 | 4 | 5;
 
 export const ColorBombDisplay = () => {
-	return <div>ColorBombDisplay</div>;
+	const [selectedCircleColorIndex, setSelectedCircleColorIndex] =
+		useState<ColorIndex>(1);
+
+	const [selectedBgColor, setSelectedBgColor] = useState<
+		(typeof colors)[number]
+	>(colors[0]);
+
+	const [circles, setCircles] = useState<
+		{
+			x: number;
+			y: number;
+			color: string;
+			id: string;
+		}[]
+	>([]);
+
+	const circlesContainerRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		if (circlesContainerRef.current) {
+			const observer = new MutationObserver((mutationsList) => {
+				for (const mutation of mutationsList) {
+					if (
+						mutation.type === "childList" &&
+						mutation.addedNodes.length > 0
+					) {
+						const addedNode = mutation.addedNodes[0];
+						if (addedNode instanceof HTMLSpanElement) {
+							setSelectedCircleColorIndex(
+								(prev) =>
+									(prev < colors.length - 1
+										? prev + 1
+										: 0) as ColorIndex
+							);
+							const intervalId = setInterval(() => {
+								addedNode.style.height = `${
+									parseFloat(addedNode.style.height) + 10
+								}px`;
+								addedNode.style.width = `${
+									parseFloat(addedNode.style.width) + 10
+								}px`;
+							}, 10);
+							setTimeout(() => {
+								setSelectedBgColor(
+									colors[selectedCircleColorIndex]
+								);
+							}, 3000);
+							setTimeout(() => {
+								clearInterval(intervalId);
+								addedNode.remove();
+							}, 3500);
+						}
+					}
+				}
+			});
+
+			observer.observe(circlesContainerRef.current, { childList: true });
+
+			return () => {
+				observer.disconnect();
+			};
+		}
+	}, [circlesContainerRef, selectedCircleColorIndex]);
+
+	const handleClick = ({ clientX, clientY }: React.MouseEvent) => {
+		setCircles((prev) => {
+			return [
+				...prev,
+				{
+					x: clientX,
+					y: clientY,
+					color: colors[selectedCircleColorIndex],
+					id: nanoid()
+				}
+			];
+		});
+	};
+
+	return (
+		<div
+			ref={circlesContainerRef}
+			style={{
+				backgroundColor: selectedBgColor
+			}}
+			onClick={handleClick}
+			className="relative flex items-center justify-center h-screen overflow-hidden font-extrabold text-center cursor-pointer select-none text-slate-950 text-9xl"
+		>
+			<span className="z-10">This can be a good header</span>
+			{circles.map(({ color, x, y, id }) => {
+				return (
+					<span
+						key={id}
+						style={{
+							top: `${y}px`,
+							left: `${x}px`,
+							backgroundColor: color,
+							height: "30px",
+							width: "30px"
+						}}
+						id={id}
+						className="absolute z-0 -translate-x-1/2 -translate-y-1/2 rounded-full"
+					/>
+				);
+			})}
+		</div>
+	);
 };
+/*
+Copied from 👇🏻
+https://humandeluxe.com/
+https://www.awwwards.com/sites/human-deluxe-design-studio
+*/
--- src/ArtWorks/index.ts
+++ src/ArtWorks/index.ts
@@ -1,11 +1,10 @@
 import { ColorBombDisplay } from "./ColorBombDisplay";
-
 import { nanoid } from "nanoid";
 export const ArtList = [
 	{
 		id: nanoid(),
 		name: "Color Bomb Display",
-		imgUrl: "https://c4.wallpaperflare.com/wallpaper/996/697/292/anime-sunset-sunrise-anime-girls-wallpaper-preview.jpg",
+		imgUrl: "../../public/colorBomdDisplay.gif",
 		path: "/color-bomb-display",
 		Element: ColorBombDisplay
 	}
