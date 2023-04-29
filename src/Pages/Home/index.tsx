67f0cfb44586e7f45913297e230b59ab6afa1856
aman-can <aman.dubey1998@gmail.com>
2023-04-30 00:27:06 +0530

    :zap: added the link tag and fixed the ArtPiece component

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -4,8 +4,14 @@ import { ArtPiece, Gallery } from "../../components";
 export const Home = () => {
 	return (
 		<Gallery>
-			{ArtList.map(({ id, imgUrl, name }) => (
-				<ArtPiece key={id} cover={imgUrl} title={name} id={id} />
+			{ArtList.map(({ id, imgUrl, name, path }) => (
+				<ArtPiece
+					key={id}
+					cover={imgUrl}
+					title={name}
+					id={id}
+					url={path}
+				/>
 			))}
 		</Gallery>
 	);
--- src/components/ArtPiece/index.tsx
+++ src/components/ArtPiece/index.tsx
@@ -1,23 +1,28 @@
+import { Link } from "react-router-dom";
+
 type Props = {
 	cover: string;
 	title: string;
 	id: string;
+	url: string;
 };
 
-export const ArtPiece = ({ cover, title, id }: Props) => {
+export const ArtPiece = ({ cover, title, id, url }: Props) => {
 	return (
-		<div
-			id={id}
-			className="relative rounded-lg w-80 aspect-video group overflow-clip"
-		>
-			<img
-				src={`${cover}`}
-				alt={title}
-				className="object-cover w-full h-full duration-700 ease-in-out group-hover:scale-110"
-			/>
-			<div className="absolute inset-0 font-thin text-center text-gray-200 duration-700 ease-in-out group-hover:backdrop-blur-sm group-hover:duration-200 group-hover:ease-linear group-hover:text-2xl group-hover:font-light">
-				{title}
+		<Link to={url}>
+			<div
+				id={id}
+				className="relative rounded-lg w-80 aspect-video group overflow-clip"
+			>
+				<img
+					src={`${cover}`}
+					alt={title}
+					className="object-cover w-full h-full duration-700 ease-in-out group-hover:scale-110"
+				/>
+				<div className="absolute inset-0 flex items-end justify-center font-thin text-gray-200 duration-700 ease-in-out cursor-pointer group-hover:items-center group-hover:backdrop-blur-lg group-hover:duration-200 group-hover:underline group-hover:ease-linear group-hover:text-2xl group-hover:font-bold">
+					{title}
+				</div>
 			</div>
-		</div>
+		</Link>
 	);
 };
