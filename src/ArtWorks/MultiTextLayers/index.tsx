4979153b6c0f322ed647572e1b7f53c2a28ae1c4
aman-can <aman.dubey1998@gmail.com>
2023-05-02 02:05:11 +0530

    🌈 Added the a art piece called Multi Text Layers

---
+++ src/ArtWorks/MultiTextLayers/index.tsx
@@ -0,0 +1,80 @@
+import { useEffect, useRef, useState } from "react";
+
+// Define the component
+export const MultiTextLayers = () => {
+	// Initialize state variables
+	const [mouseCoordinates, setMouseCoordinates] = useState({ x: 0, y: 0 });
+	const [divCenterCoordinates, setDivCenterCoordinates] = useState({
+		x: 0,
+		y: 0
+	});
+
+	// Create a reference to the div element
+	const divRef = useRef<HTMLDivElement>(null);
+
+	// Use useEffect to get the center coordinates of the div
+	useEffect(() => {
+		if (divRef.current) {
+			const div = divRef.current;
+			// Calculate the center coordinates of the div
+			const centerX = div.offsetLeft + div.offsetWidth / 2;
+			const centerY = div.offsetTop + div.offsetHeight / 2;
+			// Set the center coordinates in state
+			setDivCenterCoordinates({
+				x: centerX,
+				y: centerY
+			});
+		}
+	}, []);
+
+	// Handle mouse move events to update the mouse coordinates
+	const handleMouseMove = (e: React.MouseEvent) => {
+		// Get the width and height of the div
+		const divWidth = divRef.current?.offsetWidth || 1;
+		const divHeight = divRef.current?.offsetHeight || 1;
+		// Set the scaling factor for the mouse coordinates
+		const scaleX = 1.5;
+		const scaleY = 1.5;
+
+		// Calculate the mouse coordinates based on the position of the cursor
+		// relative to the center of the div and the scaling factors
+		setMouseCoordinates({
+			x: ((e.clientX - divCenterCoordinates.x) / divWidth) * scaleX,
+			y: ((e.clientY - divCenterCoordinates.y) / divHeight) * scaleY
+		});
+	};
+
+	// Render the component
+	return (
+		<div
+			ref={divRef}
+			onMouseMove={handleMouseMove}
+			style={{
+				// Set the text shadow based on the mouse coordinates
+				textShadow: `
+                ${mouseCoordinates.x * 8}px ${
+					mouseCoordinates.y * 8
+				}px 0px #0c0a09,
+                ${mouseCoordinates.x * 24}px ${
+					mouseCoordinates.y * 24
+				}px 0px #e11d48,
+                ${mouseCoordinates.x * 36}px ${
+					mouseCoordinates.y * 36
+				}px 0px #f97316,
+                ${mouseCoordinates.x * 48}px ${
+					mouseCoordinates.y * 48
+				}px 0px #facc15`
+			}}
+			className="flex items-center justify-center h-full uppercase bg-yellow-900 select-none font-gluten text-9xl text-neutral-50"
+		>
+			Some
+			<br />
+			Text
+		</div>
+	);
+};
+
+/*
+Copied from 👇🏻
+https://www.malihaabidi.com/
+*/
--- src/ArtWorks/index.ts
+++ src/ArtWorks/index.ts
@@ -1,4 +1,5 @@
 import { ColorBombDisplay } from "./ColorBombDisplay";
+import { MultiTextLayers } from "./MultiTextLayers";
 import { nanoid } from "nanoid";
 export const ArtList = [
 	{
@@ -7,5 +8,12 @@ export const ArtList = [
 		imgUrl: "../../public/colorBomdDisplay.gif",
 		path: "/color-bomb-display",
 		Element: ColorBombDisplay
+	},
+	{
+		id: nanoid(),
+		name: "Multi Text Layers",
+		imgUrl: "../../public/multiTextLayers.gif",
+		path: "multi-text-layers",
+		Element: MultiTextLayers
 	}
 ];
