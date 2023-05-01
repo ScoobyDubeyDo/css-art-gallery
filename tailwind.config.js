759a644f290eefe9a1af258e410979bc6fffd650
aman-can <aman.dubey1998@gmail.com>
2023-05-02 02:04:32 +0530

    🔠 Added a few new fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				frederickaTheGreat: [
+					'"Fredericka the Great"',
+					"Times New Roman"
+				],
+				gluten: ['"Gluten"', "cursive"]
+			}
+		}
 	},
 	plugins: []
 };
