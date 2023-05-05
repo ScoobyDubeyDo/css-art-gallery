6c7f7dc68980a3e78e8119d70a8b41467696051d
aman-can <aman.dubey1998@gmail.com>
2023-05-05 19:43:22 +0530

    :bug: Added the correct path to the public images

--- src/ArtWorks/index.ts
+++ src/ArtWorks/index.ts
@@ -5,14 +5,14 @@ export const ArtList = [
 	{
 		id: nanoid(),
 		name: "Color Bomb Display",
-		imgUrl: "../../public/colorBomdDisplay.gif",
+		imgUrl: "/colorBomdDisplay.gif",
 		path: "/color-bomb-display",
 		Element: ColorBombDisplay
 	},
 	{
 		id: nanoid(),
 		name: "Multi Text Layers",
-		imgUrl: "../../public/multiTextLayers.gif",
+		imgUrl: "/multiTextLayers.gif",
 		path: "multi-text-layers",
 		Element: MultiTextLayers
 	}
